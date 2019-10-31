385f24453c711cfeb834e3644977c65553aef5b2
Valdus145 <55852104+Valdus145@users.noreply.github.com>
2019-10-31 11:58:02 -0500

    Updated return

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -6,7 +6,7 @@ public abstract class MesoAbstract
 {
 	/**
 	 * Abstract method calAverage to return integer array.
-	 * @return
+	 * @return int[] Calculate the Ascii average and store the values in an array
 	 */
 	public abstract int[] calAverage();
 
